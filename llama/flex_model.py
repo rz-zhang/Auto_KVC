27bb5e046c56302db363657d7748cfe841b3b2ca
rz-zhang <ronzey.zhang@gmail.com>
2024-05-17 15:59:10 -0400

    np svd debug

--- llama/flex_model.py
+++ llama/flex_model.py
@@ -154,6 +154,7 @@ def cupy_decompose_matrix(matrix, target_dim):
 def numpy_decompose_matrix(matrix, target_dim):
     # Assuming matrix is a PyTorch tensor on a GPU, move it to CPU and convert to a NumPy array.
     matrix_np = matrix.float().cpu().numpy()
+    print('Matrix Shape:', matrix_np.shape)
 
     # Perform SVD using NumPy.
     U_np, S_np, V_np = np.linalg.svd(matrix_np, full_matrices=True)
