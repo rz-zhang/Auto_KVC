7f3f6159aebd3ced3fd8156415f8c5544678b6c1
rz-zhang <ronzey.zhang@gmail.com>
2024-05-18 21:37:28 -0400

    obqa scripts

--- example_obqa_run.sh
+++ example_obqa_run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Set the GPU to use
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=0
 
 # Base master port
 base_port=25678
 
 # Loop through dim_compress values
-for dim in {256..896..128}; do
+for dim in {768,640,128}; do
     torchrun --nproc_per_node=1 --master_port=$base_port \
         example_obqa.py \
         --ckpt_dir Meta-Llama-3-8B-Instruct/ \
         --tokenizer_path Meta-Llama-3-8B-Instruct/tokenizer.model \
-        --max_seq_len 512 --max_batch_size 10 --dim_compress $dim
+        --max_seq_len 512 --max_batch_size 100 --dim_compress $dim
 done
 
