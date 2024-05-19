a60325e2e5d99836cb758400a7889940cca674e7
rz-zhang <ronzey.zhang@gmail.com>
2024-05-19 14:02:54 -0400

    scripts

--- 70b_xsum_second_half.sh
+++ 70b_xsum_second_half.sh
@@ -5,9 +5,9 @@ trap "exit" INT
 base_port=29900
 
 # Loop through dim_compress values
-for dim in {112,96,80,64,32,16,8}; do
+for dim in {8,16,32,64,80,96,112}; do
     torchrun --nproc_per_node 8 70b_xsum.py \
     --ckpt_dir Meta-Llama-3-70B-Instruct/ \
     --tokenizer_path Meta-Llama-3-70B-Instruct/tokenizer.model \
-    --max_seq_len 2048 --max_batch_size 24 --max_gen_len 256 --dim_compress $dim --kvc_config last_20_layers
+    --max_seq_len 2048 --max_batch_size 24 --max_gen_len 256 --dim_compress $dim --kvc_config second_half
 done
